e5248297cbf2ee706547913d393b59a7aae2ad3f
Tomas Bianchini <tbianchini@frro.utn.edu.ar>
2024-02-21 08:52:41 -0300

    change declaration of the key

--- scr/users/login.controller.ts
+++ scr/users/login.controller.ts
@@ -11,8 +11,8 @@ async function login(req: Request, res: Response) {
     if (!user.verifyPassword(password)) {
       res.status(401).json({ message: "Invalid credentials" });
     }
-    const key: string | undefined = process.env.secret_key;
-    const token = jwt.sign({ id: user.id }, key ?? "", { expiresIn: "2h" });
+    const key: string | undefined = process.env.secret_key ?? "";
+    const token = jwt.sign({ id: user.id }, key, { expiresIn: "2h" });
     res.status(200).json({ message: "Login successful", data: user, token });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
