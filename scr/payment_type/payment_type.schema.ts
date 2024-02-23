d4d8fd9bb9e1a791d133f3bbb02c640bd0d777b9
Tomas Bianchini <tbianchini@frro.utn.edu.ar>
2024-02-23 15:04:19 -0300

    changed payment_type schema

--- scr/payment_type/payment_type.schema.ts
+++ scr/payment_type/payment_type.schema.ts
@@ -1,12 +1,9 @@
 import zod from "zod";
 
 const payment_typeSchema = zod.object({
-  payment_type: zod
-    .string()
-    .min(5, { message: "Payment type must be at least 5 characters long" })
-    .refine((value) => value.trim().length > 0, {
-      message: "Payment type can not be empty or contain only spaces",
-    }),
+  payment_type: zod.string().refine((value) => value.trim().length > 0, {
+    message: "Payment type can not be empty or contain only spaces",
+  }),
   state: zod
     .enum(["Archived", "Active"])
     .default("Active")
