2a2defc4b73193a8484b1eed8dc11d242ff88116
TomasBianchini <tomassbianchini@gmail.com>
2024-11-25 20:54:06 -0300

    Refactor category routes for consistency and update login controller for improved error handling and token expiration

--- scr/category/category.routes.ts
+++ scr/category/category.routes.ts
@@ -1,175 +1,21 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   findAll,
   findOne,
   add,
   update,
   remove,
-} from "./category.controller.js";
-import { auth, isAdmin } from "../middlewares/auth.js";
+} from './category.controller.js';
+import { auth, isAdmin } from '../middlewares/auth.js';
 
 export const categoryRouter = Router();
 
-/**
- * @swagger
- * /category:
- *   get:
- *     tags:
- *       - Category
- *     summary: Get all categories
- *     description: Retrieve all categories
- *     responses:
- *       200:
- *         description: Success
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                message: 
- *                 type: string
- *                data:
- *                  type: array
- *                  items:  
- *                   $ref: "#/components/schemas/category"
- *       404:
- *         description: Bad request
- *         content: {}
- *       500:
- *         description: Internal server error
- *     security:
- *       - bearerAuth: []
- */
+categoryRouter.get('/', auth, findAll);
 
-categoryRouter.get("/", auth, findAll);
+categoryRouter.get('/:id', auth, findOne);
 
-/**
- * @swagger
- * /category/:id:
- *   get:
- *     tags:
- *       - Category
- *     summary: Get category by id
- *     description: Retrieve category by id
- *     responses:
- *       200:
- *         description: Found category
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                message: 
- *                 type: string
- *                data:
- *                 $ref: "#/components/schemas/category"
- *       404:
- *         description: Not found
- *         content: {}
- *       500:
- *         description: Internal server error
- *     security:
- *       - bearerAuth: []
- */
-categoryRouter.get("/:id", auth , findOne);
-/**
- * @swagger
- * /category:
- *   post:
- *     tags:
- *       - Category
- *     summary: Create category
- *     description: Create category
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/category"
- *     responses:
- *       201:
- *         description: Cateogry created
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                message: 
- *                 type: string
- *                data:
- *                 $ref: "#/components/schemas/category"
- *       400:
- *         description: Bad request
- *         content: {}
- *     security:
- *       - bearerAuth: []
- */
+categoryRouter.post('/', isAdmin, add);
+categoryRouter.put('/:id', isAdmin, update);
+categoryRouter.patch('/:id', isAdmin, update);
 
-categoryRouter.post("/", isAdmin , add);
-
-/**
- * @swagger
- * /category/:id:
- *   put:
- *     tags:
- *       - Category
- *     summary: Update category
- *     description: Update category
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/category" 
- *     responses:
- *       200:
- *         description: Success
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                message: 
- *                 type: string
- *                data:
- *                 $ref: "#/components/schemas/category"
- *       404:
- *         description: Not found
- *         content: {}
- *       500:
- *         description: Internal server error
- *     security:
- *       - bearerAuth: []
- */
-categoryRouter.put("/:id" ,  isAdmin , update);
-categoryRouter.patch("/:id",  isAdmin, update);
-
-/**
- * @swagger
- * /category/:id:
- *   delete:
- *     tags:
- *       - Category
- *     summary: Delete category
- *     description: Delete category
- *     responses:
- *       200:
- *         description: Success
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                message: 
- *                 type: string
- *                data:
- *                 $ref: "#/components/schemas/category"
- *       404:
- *         description: Not found
- *         content: {}
- *       500:
- *         description: Internal server error
- *     security:
- *       - bearerAuth: []
- */
-categoryRouter.delete("/:id", isAdmin, remove);
+categoryRouter.delete('/:id', isAdmin, remove);
--- scr/users/login.controller.ts
+++ scr/users/login.controller.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from "express";
-import { orm } from "../shared/db/orm.js";
-import { User } from "./user.entity.js";
-import jwt from "jsonwebtoken";
-import { Seller } from "./seller.entity.js";
+import { Request, Response } from 'express';
+import { orm } from '../shared/db/orm.js';
+import { User } from './user.entity.js';
+import jwt from 'jsonwebtoken';
+import { Seller } from './seller.entity.js';
 const em = orm.em;
 
 async function login(req: Request, res: Response) {
@@ -12,24 +12,23 @@ async function login(req: Request, res: Response) {
     if (!user) {
       user = await em.findOne(Seller, { email });
       if (!user) {
-        return res.status(401).json({ message: "Invalid credentials" });
+        return res.status(401).json({ message: 'Invalid credentials' });
       }
     }
-    if(user){
+    if (user) {
       const verifyPassword = await user.verifyPassword(password);
       if (!verifyPassword) {
-        return res.status(401).json({ message: "Invalid credentials" });
+        return res.status(401).json({ message: 'Invalid credentials' });
       }
-      const key: string | undefined = process.env.secret_key ?? "";
-      const token = jwt.sign({ user: user }, key, { expiresIn: "2h" });
+      const key: string | undefined = process.env.secret_key ?? '';
+      const token = jwt.sign({ user: user }, key, { expiresIn: '7d' });
       return res
         .status(200)
-        .json({ message: "Login successful", data: user, token });
+        .json({ message: 'Login successful', data: user, token });
     }
-    } catch (error: any) {
-     res.status(500).json({ message: error.message });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
   }
 }
-
 
 export { login };
