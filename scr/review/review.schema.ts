1dd747352131f2c908075e28582ae923bac8f951
TomasBianchini <tomassbianchini@gmail.com>
2024-11-29 11:47:53 -0300

    Refactor review controller and schema to enhance error handling; integrate next middleware for error propagation and improve validation messaging with friendly error messages.

--- scr/review/review.schema.ts
+++ scr/review/review.schema.ts
@@ -1,17 +1,24 @@
-import zod from "zod";
+import zod from 'zod';
+import { friendlyMessage } from '../utils/schemas.utils.js';
+import { ValidationError } from '../shared/constants/errors.js';
 
 const reviewSchema = zod.object({
   product: zod.string(),
   comment: zod.string().max(2000),
   rating: zod.number().min(1).max(5),
   state: zod
-    .enum(["Archived", "Active"])
-    .default("Active")
-    .refine((value: string) => value === "Active" || value === "Archived", {
-      message: "State must be either Active or Archived",
+    .enum(['Archived', 'Active'])
+    .default('Active')
+    .refine((value: string) => value === 'Active' || value === 'Archived', {
+      message: 'State must be either Active or Archived',
     }),
 });
 
 export function validateReview(data: any) {
-  return reviewSchema.safeParse(data);
+  const result = reviewSchema.safeParse(data);
+  if (!result.success) {
+    const message = friendlyMessage(result);
+    throw new ValidationError(message);
+  }
+  return result;
 }
