67036c1542f568c9ff678fc9a8f4b0e9836b34ab
TomasBianchini <tomassbianchini@gmail.com>
2024-12-10 10:56:24 -0300

    Implement stock notification processing and fetch item stock from Mercado Libre API

--- scr/mercado-libre/notification/meliNotification.service.ts
+++ scr/mercado-libre/notification/meliNotification.service.ts
@@ -1,11 +1,28 @@
 import { MeliNotification } from './meliNotification.entity.js';
 import { orm } from '../../shared/db/orm.js';
+import { MeliAccount } from '../account/meliAccount.entity.js';
+import { Filter } from 'mongodb';
+import { FilterQuery } from '@mikro-orm/core';
+import axios from 'axios';
 
 const em = orm.em;
 
+const endpoint_base = process.env.MELI_API_URL;
+
 async function processStockNotification(notification: MeliNotification) {
-  console.log('Processing stock notification');
-  console.log(notification);
+  const userId = notification.userId;
+  const meliAccount = await em.findOneOrFail(MeliAccount, { userId: userId });
+  meliAccount.decryptToken();
+  getItemStock(notification.resource, meliAccount);
+}
+
+async function getItemStock(resource: string, meliAccount: MeliAccount) {
+  const response = await axios.get(`${endpoint_base}${resource}`, {
+    headers: {
+      Authorization: `Bearer ${meliAccount.accessToken}`,
+    },
+  });
+  console.log(response.data);
 }
 
 export { processStockNotification };
